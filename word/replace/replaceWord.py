c1e4bc02297a97d0f31b37955188af8ff757eaef
Elissa <elissadusabe@gmail.com>
2023-11-30 07:46:45 +0200

    program done

---
+++ word/replace/replaceWord.py
@@ -0,0 +1,7 @@
+def replace_word():
+    str = "Hey folks, I am Elissa and again hi hi hi hi"
+    word_to_replace = input("Enter the word to be replaced: ")
+    word_relacement = input("Enter to replace: ")
+    print(str.replace(word_to_replace, word_relacement))
+
+replace_word()
